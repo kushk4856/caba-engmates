6120a31f53e4276afcb46e69aac6e33d348bbbb0
kushk4856 <kushk4998@gmail.com>
2024-11-13 19:04:40 +0530

    whatsApp section updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 300,
+      modifier: 1,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    loop: true, // Enable continuous loop
+    loopedSlides: 3, // Adjust this to match how many slides should be duplicated
+    loopAdditionalSlides: 3, // Ensures proper duplication for smooth looping
+  });
+
+  // Pause autoplay on hover
+  var swiperContainer = document.querySelector('.mySwiper');
+  swiperContainer.addEventListener('mouseenter', function () {
+    swiper.autoplay.stop(); // Stop autoplay when hovering
+  });
+  swiperContainer.addEventListener('mouseleave', function () {
+    swiper.autoplay.start(); // Resume autoplay when hover ends
+  });
+});
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
   const menuToggle = document.querySelector('.menu-toggle');
   const navMenu = document.querySelector('.nav-menu');
@@ -69,7 +108,7 @@ function updateMainImage(newSrc) {
 
 // Function to update active card
 function updateActiveCard() {
-  const cards = document.querySelectorAll(".card");
+  const cards = document.querySelectorAll(".card_small");
   cards.forEach((card) => card.classList.remove("active"));
   cards[2].classList.add("active"); // Middle card is always active
 }
